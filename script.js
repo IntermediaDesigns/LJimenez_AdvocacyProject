7a20208a75e783205ef65894da6b0b5076169544
Lynjai Jimenez <139661024+IntermediaDesigns@users.noreply.github.com>
2024-03-19 21:44:41 -0400

    Update dark mode button and image file extension

--- script.js
+++ script.js
@@ -1,36 +1,35 @@
 // Toggle dark mode class
 function darkMode() {
-       var element = document.body;
-       element.classList.toggle("dark-mode");
-   }
-   
-   // Modal Image Gallery
-   function onClick(element) {
-     document.getElementById("img01").src = element.src;
-     document.getElementById("modal01").style.display = "block";
-     var captionText = document.getElementById("caption");
-     captionText.innerHTML = element.alt;
-   }
-   
-   
-   // Toggle between showing and hiding the sidebar when clicking the menu icon
-   var mySidebar = document.getElementById("mySidebar");
-   
-   function w3_open() {
-     if (mySidebar.style.display === 'block') {
-       mySidebar.style.display = 'none';
-     } else {
-       mySidebar.style.display = 'block';
-     }
-   }
-   
-   // Close the sidebar with the close button
-   function w3_close() {
-       mySidebar.style.display = "none";
-   }
-     function scrollToTop() {
-       window.scrollTo({
-         top: 0,
-         behavior: "smooth"
-       });
-     }
+  var element = document.body;
+  element.classList.toggle('dark-mode');
+}
+
+// Modal Image Gallery
+function onClick(element) {
+  document.getElementById('img01').src = element.src;
+  document.getElementById('modal01').style.display = 'block';
+  var captionText = document.getElementById('caption');
+  captionText.innerHTML = element.alt;
+}
+
+// Toggle between showing and hiding the sidebar when clicking the menu icon
+var mySidebar = document.getElementById('mySidebar');
+
+function w3_open() {
+  if (mySidebar.style.display === 'block') {
+    mySidebar.style.display = 'none';
+  } else {
+    mySidebar.style.display = 'block';
+  }
+}
+
+// Close the sidebar with the close button
+function w3_close() {
+  mySidebar.style.display = 'none';
+}
+function scrollToTop() {
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth',
+  });
+}
